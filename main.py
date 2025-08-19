d681e1116143c1c4bab56a1c4585e306ccab4c3d
Andrew Olson <ols21007@byui.edu>
2025-08-19 14:21:50 -0600

    Added car creation and set up logic for game over

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import time
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 
 screen = Screen()
+
+"""           SCREEN GRID
+                 Y| 300
+                  |
+                  |
+-300              |                300
+X-----------------|------------------
+                  |
+                  |
+                  |
+                  | -300
+"""
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car = CarManager()
 
 # Detects the arrow keys being pressed
 screen.listen()
@@ -17,3 +31,10 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    player.pos_reset()
+    car.move_car()
+
+    if player.distance(car) < 25:
+        game_is_on = False
+        
