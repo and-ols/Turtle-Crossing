43af06c765d306d4bfcba030595f7718d9125909
Andrew Olson <ols21007@byui.edu>
2025-08-19 11:57:44 -0600

    Created the player(turtle) and move functionality

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import time
 from turtle import Screen
+from player import Player
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
+
+# Detects the arrow keys being pressed
+screen.listen()
+screen.onkey(player.move_up, "Up")
+screen.onkey(player.move_down, "Down")
 
 game_is_on = True
 while game_is_on:
