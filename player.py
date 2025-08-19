1b538cd25bc8a7ff07cb81a68ea0bf31ecc0388b
Andrew Olson <ols21007@byui.edu>
2025-08-19 11:56:53 -0600

    Added creation, move and reset methods

--- player.py
+++ player.py
@@ -1,6 +1,39 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-class Player:
-    pass
+class Player(Turtle):
+    """
+    Handles all of the logic for the player controlled turtle
+
+    Methods:
+    - move_up
+    - move_down
+    - pos_reset
+    """
+
+    def __init__(self):
+        # Gets the init function from Turtle
+        super().__init__()
+
+        # Creates the turtle, sets the position and orients it the proper direction
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.left(90)
+
+    def move_up(self):
+        """Moves the turtle up by the global defined amount"""
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        """Moves the turtle down by the global defined amount"""
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def pos_reset(self):
+        """Resets the turtle to the start"""
+        self.goto(STARTING_POSITION)
