bcfd21b78d3c26e0ec793ec9f849ecede3d78952
Andrew Olson <ols21007@byui.edu>
2025-08-20 14:44:23 -0600

    Added functionality to have multiple cars, display levels and game over, and check for collisions with cars.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 
@@ -20,21 +21,50 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+scoreboard = Scoreboard()
 car = CarManager()
+
+
 
 # Detects the arrow keys being pressed
 screen.listen()
 screen.onkey(player.move_up, "Up")
 screen.onkey(player.move_down, "Down")
 
+# Initializes the counter
+count = 0
+
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(car.move_speed)
+    # Every second (or more as it increases) the count goes up and 
+    # the screen is updated
+    count += 1
     screen.update()
 
-    player.pos_reset()
+    # Displays the scoreboard
+    scoreboard.display_level()
+
+    # if the counter is divisible by 5 (so every 5 seconds) spawn cars
+    if count % 5 == 0:
+        car.add_car()
+
+    # If the player has reached the top of the screen, reset the player to the
+    # bottom and increase the speed
+    if player.reached_top():
+        scoreboard.update_level()
+        car.increase_speed()
+
+    # Continual move the cars
     car.move_car()
 
-    if player.distance(car) < 25:
-        game_is_on = False
-        
+    # Checks each cars distance from the player, if it is within 25px, stop the
+    # game loop and display the game over sign
+    for vic in car.cars:
+        if player.distance(vic) < 25:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+#Keeps the screen open until the user closes it
+screen.exitonclick()
