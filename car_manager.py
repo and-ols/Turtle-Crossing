2049dae65a54d7deff31d467c872f0b20f6fa2d6
Andrew Olson <ols21007@byui.edu>
2025-08-20 14:43:29 -0600

    Added functionality to have multiple cars, move them, and increase their speed.

--- car_manager.py
+++ car_manager.py
@@ -6,27 +6,46 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
     """
     Handles all of the logic for the cars passing on the screen
 
     Methods:
+    - add_car
+    - create_car
     - move_car
+    - increase_speed
     """
+
     def __init__(self):
-        # Gets the init function from Turtle
-        super().__init__()
+        self.cars = []
+        self.move_speed = .1
 
-        # Creates the car, sets the shape and gets a random position and color for it
-        self.shape("square")
-        self.penup()
-        self.left(180)
-        self.color(random.choice(COLORS))
-        self.shapesize(stretch_wid=1, stretch_len=2)
-        # self.goto(300, random.randrange(-240, 280 + 1))
-        self.goto(300, -240)
+    def add_car(self):
+        """
+        Creates a car, gives it a random color, and location and adds it to
+        the list of cars
+        """
+        new_car = Turtle(shape="square")
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.goto(320, random.randrange(-240, 280 + 1))
+        self.cars.append(new_car)
 
+    def create_car(self):
+        """Calls add car to create 5 cars using a for loop"""
+        for car in range(5):
+             self.add_car()
+    
     def move_car(self):
         """Moves the car left by the global defined amount"""
-        # Subtract the move distance because it starts at positive 300
-        self.goto(self.xcor() - STARTING_MOVE_DISTANCE, self.ycor())
+        for car in range(len(self.cars)):
+            # Move backwards because it starts at positive 300
+            self.cars[car].backward(STARTING_MOVE_DISTANCE)
+
+    def increase_speed(self):
+        """Increases the speed of the cars"""
+        # Divide because the smaller the time number, the faster the time
+        self.move_speed /= MOVE_INCREMENT
+
